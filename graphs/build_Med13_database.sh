942830104f8848eb736df146e446d408799fa72a
eead-csic-compbio <bcontreras@eead.csic.es>
2025-10-02 11:55:07 +0200

    renamed Med13

--- graphs/build_Med13_database.sh
+++ graphs/build_Med13_database.sh
@@ -55,17 +55,17 @@ echo "Using gmap_build version: $gmap_build_version"
 # Commented to avoid it during tests
 
 # Generate necessary folders
-mkdir -p Med11/data
-mkdir -p Med11/output
-mkdir -p Med11/vcf_dbs
-mkdir -p Med11/output/alignment_files
-mkdir -p Med11/output/vcf_files
-mkdir -p Med11/output/read_mapping
-mkdir -p Med11/output/imputed_vcf_files
+mkdir -p Med13/data
+mkdir -p Med13/output
+mkdir -p Med13/vcf_dbs
+mkdir -p Med13/output/alignment_files
+mkdir -p Med13/output/vcf_files
+mkdir -p Med13/output/read_mapping
+mkdir -p Med13/output/imputed_vcf_files
 mkdir -p gmap_db
 
 # Initialize the database
-phg initdb --db-path Med11/vcf_dbs/
+phg initdb --db-path Med13/vcf_dbs/
 
 # Get the genomes
 # from the folder:
@@ -86,58 +86,58 @@ phg initdb --db-path Med11/vcf_dbs/
 # https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783505.1?download=true&gzip=true   HOR_12184
 # https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_904849725.1?download=true&gzip=true   MorexV3
 
-if [ ! -f Med11/data/HOR_14121.fasta.gz ] && [ ! -f Med11/data/HOR_14121.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783365.1?download=true&gzip=true" -O Med11/data/HOR_14121.fasta.gz
+if [ ! -f Med13/data/HOR_14121.fasta.gz ] && [ ! -f Med13/data/HOR_14121.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783365.1?download=true&gzip=true" -O Med13/data/HOR_14121.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_21595.fasta.gz ] && [ ! -f Med11/data/HOR_21595.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783375.1?download=true&gzip=true" -O Med11/data/HOR_21595.fasta.gz
+if [ ! -f Med13/data/HOR_21595.fasta.gz ] && [ ! -f Med13/data/HOR_21595.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783375.1?download=true&gzip=true" -O Med13/data/HOR_21595.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_3474.fasta.gz ] && [ ! -f Med11/data/HOR_3474.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783475.1?download=true&gzip=true" -O Med11/data/HOR_3474.fasta.gz
+if [ ! -f Med13/data/HOR_3474.fasta.gz ] && [ ! -f Med13/data/HOR_3474.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783475.1?download=true&gzip=true" -O Med13/data/HOR_3474.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_13942.fasta.gz ] && [ ! -f Med11/data/HOR_13942.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783295.1?download=true&gzip=true" -O Med11/data/HOR_13942.fasta.gz
+if [ ! -f Med13/data/HOR_13942.fasta.gz ] && [ ! -f Med13/data/HOR_13942.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783295.1?download=true&gzip=true" -O Med13/data/HOR_13942.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_21599.fasta.gz ] && [ ! -f Med11/data/HOR_21599.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783435.1?download=true&gzip=true" -O Med11/data/HOR_21599.fasta.gz
+if [ ! -f Med13/data/HOR_21599.fasta.gz ] && [ ! -f Med13/data/HOR_21599.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783435.1?download=true&gzip=true" -O Med13/data/HOR_21599.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_3365.fasta.gz ] && [ ! -f Med11/data/HOR_3365.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783415.1?download=true&gzip=true" -O Med11/data/HOR_3365.fasta.gz
+if [ ! -f Med13/data/HOR_3365.fasta.gz ] && [ ! -f Med13/data/HOR_3365.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783415.1?download=true&gzip=true" -O Med13/data/HOR_3365.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_2830.fasta.gz ] && [ ! -f Med11/data/HOR_2830.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783545.1?download=true&gzip=true" -O Med11/data/HOR_2830.fasta.gz
+if [ ! -f Med13/data/HOR_2830.fasta.gz ] && [ ! -f Med13/data/HOR_2830.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783545.1?download=true&gzip=true" -O Med13/data/HOR_2830.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_10892.fasta.gz ] && [ ! -f Med11/data/HOR_10892.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783585.1?download=true&gzip=true" -O Med11/data/HOR_10892.fasta.gz
+if [ ! -f Med13/data/HOR_10892.fasta.gz ] && [ ! -f Med13/data/HOR_10892.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783585.1?download=true&gzip=true" -O Med13/data/HOR_10892.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_2779.fasta.gz ] && [ ! -f Med11/data/HOR_2779.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783485.1?download=true&gzip=true" -O Med11/data/HOR_2779.fasta.gz
+if [ ! -f Med13/data/HOR_2779.fasta.gz ] && [ ! -f Med13/data/HOR_2779.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783485.1?download=true&gzip=true" -O Med13/data/HOR_2779.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_1168.fasta.gz ] && [ ! -f Med11/data/HOR_1168.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783555.1?download=true&gzip=true" -O Med11/data/HOR_1168.fasta.gz
+if [ ! -f Med13/data/HOR_1168.fasta.gz ] && [ ! -f Med13/data/HOR_1168.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783555.1?download=true&gzip=true" -O Med13/data/HOR_1168.fasta.gz
 fi
-if [ ! -f Med11/data/HOR_12184.fasta.gz ] && [ ! -f Med11/data/HOR_12184.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783505.1?download=true&gzip=true" -O Med11/data/HOR_12184.fasta.gz
+if [ ! -f Med13/data/HOR_12184.fasta.gz ] && [ ! -f Med13/data/HOR_12184.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_949783505.1?download=true&gzip=true" -O Med13/data/HOR_12184.fasta.gz
 fi
-if [ ! -f Med11/data/MorexV3.fasta.gz ] && [ ! -f Med11/data/MorexV3.fa ]; then
-    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_904849725.1?download=true&gzip=true" -O Med11/data/MorexV3.fasta.gz
+if [ ! -f Med13/data/MorexV3.fasta.gz ] && [ ! -f Med13/data/MorexV3.fa ]; then
+    wget "https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_904849725.1?download=true&gzip=true" -O Med13/data/MorexV3.fasta.gz
 fi
 
 ## From the own assembly of GDB136, which is not yet published:
 
-#    scp /scratch/GDB136/IPK/output/pseudomolecules_v1/pseudomolecules_v1.fasta Med11/data/GDB_136_raw.fa
-#    cat Med11/data/GDB_136_raw.fa | perl -lne 'if (/^>GDB\d+_(chr\S+)/) { print ">$1" } elsif (/^>/) { last } else { print }' > Med11/data/GDB_136.fa
-#    rm Med11/data/GDB_136_raw.fa
-#    mv Med11/data/GDB_136.fa Med11/data/GDB_136_raw.fa
-#    rm Med11/data/prepare_assemblies.txt
-#    echo -e "Med11/data/GDB_136_raw.fa\tGDB_136" > Med11/data/prepare_assemblies.txt
-#    phg prepare-assemblies --keyfile Med11/data/prepare_assemblies.txt --output-dir Med11/data/ --threads 32
+#    scp /scratch/GDB136/IPK/output/pseudomolecules_v1/pseudomolecules_v1.fasta Med13/data/GDB_136_raw.fa
+#    cat Med13/data/GDB_136_raw.fa | perl -lne 'if (/^>GDB\d+_(chr\S+)/) { print ">$1" } elsif (/^>/) { last } else { print }' > Med13/data/GDB_136.fa
+#    rm Med13/data/GDB_136_raw.fa
+#    mv Med13/data/GDB_136.fa Med13/data/GDB_136_raw.fa
+#    rm Med13/data/prepare_assemblies.txt
+#    echo -e "Med13/data/GDB_136_raw.fa\tGDB_136" > Med13/data/prepare_assemblies.txt
+#    phg prepare-assemblies --keyfile Med13/data/prepare_assemblies.txt --output-dir Med13/data/ --threads 32
 
 # Check if there are files ending in .fasta.gz in the /data/ directory
-if ls Med11/data/*.fasta.gz 1> /dev/null 2>&1; then
+if ls Med13/data/*.fasta.gz 1> /dev/null 2>&1; then
     # Unzip and change the chromosome names for all files in /data/ ending with .fasta.gz
-    for genome_file in Med11/data/*.fasta.gz; do
-        output_file="Med11/data/$(basename "$genome_file" .fasta.gz).fa"
+    for genome_file in Med13/data/*.fasta.gz; do
+        output_file="Med13/data/$(basename "$genome_file" .fasta.gz).fa"
         sample_name=$(basename "$genome_file" .fasta.gz)
         if [ ! -f "$output_file" ]; then
             zcat "$genome_file" | perl -lne 'if(/^>ENA\|[^|]+\|(\S+).*?chromosome: (\S+)/){ print ">chr$2 sampleName='"$sample_name"'" } else { last if(/^>/); print }' > "$output_file"
@@ -145,16 +145,16 @@ if ls Med11/data/*.fasta.gz 1> /dev/null 2>&1; then
         fi
     done
 else
-    echo "No .fasta.gz files found in the Med11/data/ directory. Skipping this step."
+    echo "No .fasta.gz files found in the Med13/data/ directory. Skipping this step."
 fi
 
 
 # # Execute prepare assemblies from PHG (but only if there is a file that should exists that does not)    ## Uses 32 threads
 # for genome_file in "${genome_files[@]}"; do
-#    output_file="Med11/data/prepared_assemblies/$(basename "$genome_file" .fasta.gz).fa"
+#    output_file="Med13/data/prepared_assemblies/$(basename "$genome_file" .fasta.gz).fa"
 #    if [ ! -f "$output_file" ]; then
-#        echo "phg prepare_assemblies --keyfile Med11/data/prepare_assemblies.txt --output-dir Med11/data/prepared_assemblies/ --threads 32"
-#        phg prepare-assemblies --keyfile Med11/data/prepare_assemblies.txt --output-dir Med11/data/prepared_assemblies/ --threads 32
+#        echo "phg prepare_assemblies --keyfile Med13/data/prepare_assemblies.txt --output-dir Med13/data/prepared_assemblies/ --threads 32"
+#        phg prepare-assemblies --keyfile Med13/data/prepare_assemblies.txt --output-dir Med13/data/prepared_assemblies/ --threads 32
 #        break
 #    fi
 #done
@@ -163,43 +163,43 @@ fi
 
 ### Download Morex.gff
 
-if [ ! -f Med11/data/MorexV3.gff ]; then
-    wget "https://doi.ipk-gatersleben.de/DOI/b2f47dfb-47ff-4114-89ae-bad8dcc515a1/5d16cc17-c37f-417f-855d-c5e72c721f6c/1/DOWNLOAD" -O Med11/data/RAW_MorexV3.gff
+if [ ! -f Med13/data/MorexV3.gff ]; then
+    wget "https://doi.ipk-gatersleben.de/DOI/b2f47dfb-47ff-4114-89ae-bad8dcc515a1/5d16cc17-c37f-417f-855d-c5e72c721f6c/1/DOWNLOAD" -O Med13/data/RAW_MorexV3.gff
 fi
 # Not necessary to modify the gff due chromosomes names: already match with the prepared fasta files
 # But remove all is not in 7 chromosomes
 
-if [ ! -f Med11/data/MorexV3.gff ]; then
-    perl -ne 'if(/^chr[1-7]H\t/){print}' Med11/data/RAW_MorexV3.gff > Med11/data/MorexV3.gff
-    rm Med11/data/RAW_MorexV3.gff
+if [ ! -f Med13/data/MorexV3.gff ]; then
+    perl -ne 'if(/^chr[1-7]H\t/){print}' Med13/data/RAW_MorexV3.gff > Med13/data/MorexV3.gff
+    rm Med13/data/RAW_MorexV3.gff
 fi
 
 # Prepare reference-ranges
-if [ ! -f Med11/output/ref_ranges.bed ]; then
-    phg create-ranges --gff Med11/data/MorexV3.gff --boundary gene -o Med11/output/ref_ranges.bed --reference-file Med11/data/MorexV3.fa
+if [ ! -f Med13/output/ref_ranges.bed ]; then
+    phg create-ranges --gff Med13/data/MorexV3.gff --boundary gene -o Med13/output/ref_ranges.bed --reference-file Med13/data/MorexV3.fa
 fi
 
 
 # if exists the keyfile for the aligment, delete it
-if [ -f Med11/data/alignment_keyfile.txt ]; then
-    rm Med11/data/alignment_keyfile.txt
+if [ -f Med13/data/alignment_keyfile.txt ]; then
+    rm Med13/data/alignment_keyfile.txt
 fi
 
 # Create the keyfile for the alignment
-# Check if the .fa files exist in the Med11/data/ directory
+# Check if the .fa files exist in the Med13/data/ directory
 # If they do, create the keyfile, but not "creating" a "*.fa" file
-if ls Med11/data/*.fa 1> /dev/null 2>&1; then
-    for prepared_file in Med11/data/*.fa; do
+if ls Med13/data/*.fa 1> /dev/null 2>&1; then
+    for prepared_file in Med13/data/*.fa; do
         if [[ "$(basename "$prepared_file")" != "MorexV3.fa" ]]; then
-            echo "$prepared_file" >> Med11/data/alignment_keyfile.txt
+            echo "$prepared_file" >> Med13/data/alignment_keyfile.txt
         fi
     done
 else
-    echo "No .fa files found in the Med11/data/ directory."
+    echo "No .fa files found in the Med13/data/ directory."
 fi
 
 # Save in a variable the genome files
-genome_files=(Med11/data/*.fa)
+genome_files=(Med13/data/*.fa)
 
 # Align assemblies
 # Execute align_assemblies from PHG (but only if there is a file that should exists that does not)    ## Uses 32 threads
@@ -207,8 +207,8 @@ for genome_file in "${genome_files[@]}"; do
     if [[ "$(basename "$genome_file" .fa)" == "MorexV3" ]]; then
         continue
     fi
-    if [ ! -f "Med11/output/alignment_files/$(basename "$genome_file" .fa).maf" ]; then
-        phg align-assemblies --gff Med11/data/MorexV3.gff -o Med11/output/alignment_files/ --total-threads 32 --in-parallel 2 --assembly-file-list Med11/data/alignment_keyfile.txt --output-dir Med11/output/alignment_files/ --reference-file Med11/data/MorexV3.fa
+    if [ ! -f "Med13/output/alignment_files/$(basename "$genome_file" .fa).maf" ]; then
+        phg align-assemblies --gff Med13/data/MorexV3.gff -o Med13/output/alignment_files/ --total-threads 32 --in-parallel 2 --assembly-file-list Med13/data/alignment_keyfile.txt --output-dir Med13/output/alignment_files/ --reference-file Med13/data/MorexV3.fa
         break
     fi
 done
@@ -219,20 +219,20 @@ for genome_file in "${genome_files[@]}"; do
     if [[ "$(basename "$genome_file" .fa)" == "MorexV3" ]]; then
         continue
     fi
-    if [ ! -f "Med11/output/alignment_files/$(basename "$genome_file" .fa).maf" ]; then
+    if [ ! -f "Med13/output/alignment_files/$(basename "$genome_file" .fa).maf" ]; then
         echo "Alignment files not created for $(basename "$genome_file" .fa). Exiting script."
         exit 1
     fi
 done
 
 # Build the agc file to store the genomes
-if [ ! -f Med11/vcf_dbs/assemblies.agc ]; then
-    phg agc-compress --db-path Med11/vcf_dbs/ --fasta-list Med11/data/alignment_keyfile.txt --reference-file Med11/data/MorexV3.fa
+if [ ! -f Med13/vcf_dbs/assemblies.agc ]; then
+    phg agc-compress --db-path Med13/vcf_dbs/ --fasta-list Med13/data/alignment_keyfile.txt --reference-file Med13/data/MorexV3.fa
 fi
 
 # Create the reference VCF
-if [ ! -f Med11/vcf_dbs/hvcf_files/MorexV3.h.vcf.gz ]; then
-    phg create-ref-vcf --bed Med11/output/ref_ranges.bed --reference-file Med11/data/MorexV3.fa --reference-name MorexV3 --db-path Med11/vcf_dbs/
+if [ ! -f Med13/vcf_dbs/hvcf_files/MorexV3.h.vcf.gz ]; then
+    phg create-ref-vcf --bed Med13/output/ref_ranges.bed --reference-file Med13/data/MorexV3.fa --reference-name MorexV3 --db-path Med13/vcf_dbs/
 fi
 
 # Create from allignments the VCFs
@@ -241,37 +241,37 @@ for genome_file in "${genome_files[@]}"; do
     if [[ "$(basename "$genome_file" .fa)" == "MorexV3" ]]; then
         continue
     fi
-    if [ ! -f "Med11/output/vcf_files/$(basename "$genome_file" .fa).h.vcf.gz" ]; then
-        phg create-maf-vcf --bed Med11/output/ref_ranges.bed --reference-file Med11/data/MorexV3.fa -o Med11/output/vcf_files/ --db-path Med11/vcf_dbs/ --skip-metrics --maf-dir Med11/output/alignment_files/
+    if [ ! -f "Med13/output/vcf_files/$(basename "$genome_file" .fa).h.vcf.gz" ]; then
+        phg create-maf-vcf --bed Med13/output/ref_ranges.bed --reference-file Med13/data/MorexV3.fa -o Med13/output/vcf_files/ --db-path Med13/vcf_dbs/ --skip-metrics --maf-dir Med13/output/alignment_files/
     fi
 done
 
 # Set a checkpoint to see if the files are already created
 for genome_file in "${genome_files[@]}"; do
-    output_file="Med11/data/$(basename "$genome_file" .fa).fa"
+    output_file="Med13/data/$(basename "$genome_file" .fa).fa"
 
-    if [ ! -f "Med11/vcf_dbs/hvcf_files/$(basename "$genome_file" .fa).h.vcf.gz" ]; then
-        phg load-vcf --db-path Med11/vcf_dbs/ --vcf-dir Med11/output/vcf_files/ --threads 32
+    if [ ! -f "Med13/vcf_dbs/hvcf_files/$(basename "$genome_file" .fa).h.vcf.gz" ]; then
+        phg load-vcf --db-path Med13/vcf_dbs/ --vcf-dir Med13/output/vcf_files/ --threads 32
         break
     fi
 done
 
 
 # Generate necessary files: hapIDranges:
-if [ ! -f Med11/output/hapIDranges.tsv ]; then
-    phg sample-hapid-by-range --input-dir Med11/vcf_dbs/hvcf_files/ --output-file Med11/output/hapIDranges.tsv
+if [ ! -f Med13/output/hapIDranges.tsv ]; then
+    phg sample-hapid-by-range --input-dir Med13/vcf_dbs/hvcf_files/ --output-file Med13/output/hapIDranges.tsv
 fi
 
 # Generate necessary files: hapIDranges:
-if [ ! -f Med11/output/hapIDtable.tsv ]; then
-    phg hapid-sample-table --hvcf-dir Med11/vcf_dbs/hvcf_files/ --output-file Med11/output/hapIDtable.tsv
+if [ ! -f Med13/output/hapIDtable.tsv ]; then
+    phg hapid-sample-table --hvcf-dir Med13/vcf_dbs/hvcf_files/ --output-file Med13/output/hapIDtable.tsv
 fi
 
 # Generate the gmap database
 for genome_file in "${genome_files[@]}"; do
     genome_name=$(basename "$genome_file" .fa)
     if [ ! -f "gmap_db/${genome_name}/${genome_name}.chromosome" ]; then
-        $gmap_build_command -D gmap_db/ -d "${genome_name}" "Med11/data/${genome_name}.fa"
+        $gmap_build_command -D gmap_db/ -d "${genome_name}" "Med13/data/${genome_name}.fa"
     fi
 done
 
@@ -289,14 +289,14 @@ done
 #echo "Java memory set to:"
 #echo $JAVA_OPTS
 
-#if [ ! -f Med11/outputkmerIndex.txt ]; then
-#    phg build-kmer-index --db-path Med11/vcf_dbs/ --index-file Med11/outputkmerIndex.txt --hvcf-dir Med11/vcf_dbs/hvcf_files/ --use-big-discard-set
+#if [ ! -f Med13/outputkmerIndex.txt ]; then
+#    phg build-kmer-index --db-path Med13/vcf_dbs/ --index-file Med13/outputkmerIndex.txt --hvcf-dir Med13/vcf_dbs/hvcf_files/ --use-big-discard-set
 #fi
 ########
 
 ### Preparing kmer mapping in BETA
-if [ ! -f Med11/output/ropeBWT_index.fmd ]; then
-    phg rope-bwt-index --hvcf-dir Med11/vcf_dbs/hvcf_files/ --db-path Med11/vcf_dbs/ --output-dir Med11/output --index-file-prefix ropeBWT_index --threads 32
+if [ ! -f Med13/output/ropeBWT_index.fmd ]; then
+    phg rope-bwt-index --hvcf-dir Med13/vcf_dbs/hvcf_files/ --db-path Med13/vcf_dbs/ --output-dir Med13/output --index-file-prefix ropeBWT_index --threads 32
 fi
 
 #######
@@ -386,26 +386,17 @@ fi
 # Create the bed files for each genome
 for genome_file in "${genome_files[@]}"; do
     genome_name=$(basename "$genome_file" .fa)
-    if [ ! -f "Med11/vcf_dbs/hvcf_files/${genome_name}.h.bed" ]; then
+    if [ ! -f "Med13/vcf_dbs/hvcf_files/${genome_name}.h.bed" ]; then
         echo "Creating bed file for $genome_name"
-        python3 hvcf2bed.py Med11/vcf_dbs/hvcf_files/ "$genome_name"
+        python3 hvcf2bed.py Med13/vcf_dbs/hvcf_files/ "$genome_name"
     fi
 done
 
 # Index the fasta files
 for genome_file in "${genome_files[@]}"; do
     genome_name=$(basename "$genome_file" .fa)
-    if [ ! -f "Med11/data/${genome_name}.fa.fai" ]; then
+    if [ ! -f "Med13/data/${genome_name}.fa.fai" ]; then
         echo "Indexing fasta file for $genome_name"
-        samtools faidx "Med11/data/${genome_name}.fa"
+        samtools faidx "Med13/data/${genome_name}.fa"
     fi
 done
-
-# Prepare to work with align2graph
-
-# If align2graph.py is not present, download it
-if [ ! -f align2graph.py ]; then
-    echo "Downloading align2graph.py script"
-    wget "https://raw.githubusercontent.com/eead-csic-compbio/eead-csic-compbio.github.io/master/scripts/align2graph.py" -O align2graph.py
-fi
-
